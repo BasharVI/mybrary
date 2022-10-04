c9103889eae657ff3cd8d06c6b7a318526273748
Bashar V I <basharvismail@gmail.com>
2022-10-04 17:08:02 +0530

    author edit/view/del completed but still minor errors

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
  name: {
@@ -6,6 +7,19 @@ const authorSchema = new mongoose.Schema({
     required:true,
     validate: /\S+/
  }
+})
+authorSchema.pre('remove', async function(next){
+   console.log(this.id);
+   let books=await Book.find({author:this.id})
+      console.log(books);
+      if (err){
+         next(err)
+      } else if (books.length>0){
+         next(new Error('The Author still have books'))
+      } else {
+         next()
+      }
+   
 })
 
 module.exports = mongoose.model('Author', authorSchema)
